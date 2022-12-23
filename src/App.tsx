a733a0b2ffdd012732d9f420aeb6ccef455dde8c
Oscar Vasquez <oskydiverps@gmail.com>
2022-12-22 21:47:17 -0800

    add email

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <h3>OVC</h3>
       </header>
       <main className="App-main">
+        <a href="mailto:oskydiverps@gmail.com">Email Me</a>
       </main>
     </div>
   );
