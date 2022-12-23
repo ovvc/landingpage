bea67eab54bd2b376d479d0b5a478e9a85daaa45
Oscar Vasquez <oskydiverps@gmail.com>
2022-12-22 21:37:26 -0800

    first version

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <h3>OVC</h3>
+      </header>
+      <main className="App-main">
+      </main>
+    </div>
+  );
+}
+
+
+const Button = ({ label = 'Click me!' }) => {
+  return <button>{label}</button>
+}
+
+export default App;
